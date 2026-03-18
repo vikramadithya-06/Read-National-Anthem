fb2db532268bc13f71c466fb6297883ea981c05a
vikramadithya-06 <vikramadithyat51@gmail.com>
2026-03-18 23:02:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-console.log("JS loaded");
 const menuBtns = document.querySelectorAll(".menu-btn");
-const sidebar = document.querySelector(".sidebar");
+const sidebar = document.querySelector(".sidebars");
 
-if (menuBtns.length && sidebar) {
+if (menuBtns.length && sidebars) {
   menuBtns.forEach(btn => {
     btn.addEventListener("click", () => {
       sidebar.classList.toggle("active");
