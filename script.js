7b0d2b3c9daa96ff87bedb38b239830f6c529b1e
vikramadithya-06 <vikramadithyat51@gmail.com>
2026-03-19 21:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuBtns = document.querySelectorAll(".menu-button");
 const sidebar = document.querySelector(".sidebars");
 
-if (menuBtns.length && sidebars) {
+if (menuBtns.length && sidebar) {
   menuBtns.forEach(btn => {
     btn.addEventListener("click", () => {
       sidebar.classList.toggle("active");
